7cdeb24e9e0690fbc3e2c252a3a22734a07f830a
Matvei Nikita <matveinikitaivanovich@gmail.com>
2024-02-22 23:10:35 +0200

    Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,16 @@
 package org.example;
 
-import org.example.model.Director;
-import org.example.model.Movie;
+import org.example.model.Passport;
+import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.sql.ast.tree.expression.Collation;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.stream.Collectors;
-
 
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().configure()
-                .addAnnotatedClass(Director.class)
-                .addAnnotatedClass(Movie.class);
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Passport.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -24,15 +18,13 @@ public class App {
         try {
             session.beginTransaction();
 
+            Person person = session.get(Person.class, 1);
+            Passport passport = new Passport(12344);
+            person.setPassport(passport);
 
-
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
-            if (sessionFactory != null && !sessionFactory.isClosed()) {
-                sessionFactory.close();
-            }
+            session.getTransaction().commit();
+        }finally {
+            sessionFactory.close();
         }
     }
 }
---
+++ src/main/java/org/example/model/Person.java
@@ -0,0 +1,53 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cascade;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(mappedBy = "person")
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    private Passport passport;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+        passport.setPerson(this);
+    }
+}
